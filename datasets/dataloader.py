9822712b590dca8f94a9aa5beed18d2ee85f1c33
jjung-ah <71995801+jjung-ah@users.noreply.github.com>
2022-01-03 17:47:39 +0900

    Update dataloader.py

--- datasets/dataloader.py
+++ datasets/dataloader.py
@@ -1,35 +1,34 @@
 from torch.utils.data import DataLoader
-from torchvision import transforms
-from jjung.datasets import BasicDataset
+from datasets import BasicDataset
 
-from jjung.datasets.utils import make_train_val
-from jjung.transform.augmentation import Augmentation
+from datasets.utils import make_train_test
+from transform.transform import _train_transforms, _test_transforms
+from transform.augmentation import Augmentation
 
 from hydra import compose, initialize
 from omegaconf import OmegaConf
 
-initialize(config_path="conf", job_name="test_app")
-cfg = compose(config_name="config", overrides=["db=mysql", "db.user=me"])
+initialize(congig_path="conf", job_name="test_app")  # 이 부분 수정
+cfg = compose(config_name="config", overrides=["data.image_dir", "data.label_dir", "data.train_ratio=0.6"])
 print(OmegaConf.to_yaml(cfg))
 
 
-def build_train_dataset(cfg):
-    TRAIN_LIST, VAL_LIST = make_train_val(
-        image_dir = cfg.data.image_dir,  # data에 대한 hydra yaml파일 만들어야함
-        label_dir = cfg.data.label_dir,
-        train_ratio = cfg.data.train_ratio,
-        shuffle = cfg.data.shuffle
+def build_dataset(cfg):
+    TRAIN_LIST, TEST_LIST = make_train_test(
+        image_dir=cfg.data.root_dir,   # data에 대한 hydra yaml파일 만들어야함
+        train_ratio=cfg.data.train_ratio,
+        shuffle=cfg.data.shuffle
     )
 
     if cfg.data.augmentation:
-        TRAIN_LSIT, VAL_LIST = augmentation(
-            train_set = TRAIN_LIST, val_set = VAL_LIST, data_dir = cfg.data.image_dir
+        TRAIN_LIST, TEST_LIST = Augmentation(
+            train_set=TRAIN_LIST, val_set=TEST_LIST, data_dir=cfg.data.image_dir
         )
-      
-    train_datasets = BasicDataset(TRAIN_LIST, transform=_train_trainsforms())
-    val_datasets = BasicDataset(VAL_LIST, transform=_val_transforms())
 
-    train_loader = DataLoader(train_datasets, batch_size=cfg.train.batch_size, drop_last = cfg.data.drop_last)
-    val_loader = DataLoader(val_datasets, batch_size=1, drop_last = cfg.data.drop_last)
+    train_datasets = BasicDataset(TRAIN_LIST, transform=_train_transforms())
+    test_datasets = BasicDataset(TEST_LIST, transform=_test_transforms())
 
-    return train_loader, val_loader
+    train_loader = DataLoader(train_datasets, batch_size=cfg.train.batch_size, drop_last=cfg.data.drop_last)
+    test_loader = DataLoader(test_datasets, batch_size=1, drop_last=cfg.data.drop_last)
+
+    return train_loader, test_loader
