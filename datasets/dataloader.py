c0cefc7d0ab2bd221a2b3842eae73b8e811d327f
jjung-ah <71995801+jjung-ah@users.noreply.github.com>
2022-01-05 17:33:04 +0900

    Update dataloader.py

--- datasets/dataloader.py
+++ datasets/dataloader.py
@@ -6,16 +6,16 @@ from transform.transform import _train_transforms, _test_transforms
 from transform.augmentation import Augmentation
 
 from hydra import compose, initialize
-from omegaconf import OmegaConf
+from omegaconf import DictConfig
+# from omegaconf import OmegaConf
 # from .checker import check_file
 
+initialize(congig_path="configs", config_name="config")  # 이 부분 수정
+cfg = compose(config_name="config", overrides=["data.root_dir", "data.train_ratio=0.6"])   # 여기도 밑에 더 쓰이고 있는 drop_last 등 추가할 부분 확인
+# print(OmegaConf.to_yaml(cfg))
 
-initialize(congig_path="conf", job_name="test_app")  # 이 부분 수정
-cfg = compose(config_name="config", overrides=["data.image_dir", "data.label_dir", "data.train_ratio=0.6"])
-print(OmegaConf.to_yaml(cfg))
 
-
-def build_dataset(cfg):
+def build_dataset(cfg: DictConfig):
     train_list, test_list = make_train_test(
         image_dir=cfg.data.root_dir,   # data에 대한 hydra yaml파일 만들어야함
         train_ratio=cfg.data.train_ratio,
@@ -24,7 +24,7 @@ def build_dataset(cfg):
 
     if cfg.data.augmentation:
         train_list, test_list = Augmentation(
-            train_set=train_list, val_set=test_list, data_dir=cfg.data.image_dir
+            train_set=train_list, val_set=test_list, data_dir=cfg.data.image_dir   # image_dir 인지 root_dir 인지 다시 한번 확인 필요
         )
 
     # TRAIN_LIST, TEST_LIST = check_file(TRAIN_LIST, TEST_LIST)
